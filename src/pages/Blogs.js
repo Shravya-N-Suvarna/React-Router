751089dfc9084e075c48a944d5ae89ca97a73c5f
Shravya <shravyasuvarna1998@gmail.com>
2024-06-08 12:56:52 +0530

    master

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Blogs() {
+  // const[label,setlabel]=useState(localStorage.getItem("name"))
+  const [name, setName] = useState("");
+  const handleSubmit = () => { 
+    localStorage.setItem("name", name);
+    }
   return (
-    <div>Blogs</div>
+    <div>
+      <form onClick={handleSubmit}>
+        <label>Enter the Name: </label>
+        <input type="text" name="name" onChange={(e) => {setName(e.target.value);
+        }}/>
+        <button type="submit">Submit</button>
+      </form>
+    <h1>{localStorage.getItem("name")}</h1>
+    </div>
   )
 }
--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
 export default function NoPage() {
+  
   return (
-    <div>NoPage</div>
+    <div>
+      
+    </div>
   )
 }
